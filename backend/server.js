7464d3c744df6ad8bd461389353fab058131265d
haninnouh <hanin.noah@tuwaiq.edu.sa>
2021-12-21 02:19:15 +0300

    test7

--- backend/server.js
+++ backend/server.js
@@ -13,13 +13,14 @@ const app = express();
 var cors = require("cors");
 require("dotenv").config()
 const path = require("path");
-__dirname = path.resolve();
+const __variableOfChoice = path.resolve()
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "./frontend/build")));
+  app.use('/', express.static(path.join(__variableOfChoice , 
+    '/frontend/build')))
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    res.sendFile(path.join(__variableOfChoice, "frontend", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
