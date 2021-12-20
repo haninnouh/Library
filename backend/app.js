d672f9d701c9cba5c71db782c5478365b333c46e
haninnouh <hanin.noah@tuwaiq.edu.sa>
2021-12-21 01:00:36 +0300

    test6

--- backend/app.js
+++ backend/app.js
@@ -248,10 +248,10 @@ var ObjectId = require("mongodb").ObjectID;
 // });
 
 // Step 1:
-app.use(express.static(path.resolve(__dirname, "./frontend/build")));
+app.use(express.static(path.resolve(__dirname, "/frontend/build")));
 // Step 2:
 app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./frontend/build", "index.html"));
+  response.sendFile(path.resolve(__dirname, "/frontend/build", "index.html"));
 });
 const PORT = process.env.PORT || 3001;
 app.listen(PORT);
